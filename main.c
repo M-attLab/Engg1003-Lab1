8dcd196a811257e22643a98f25f89758807081b6
Matt Halliwell <c3349413@uon.edu.au>
2020-02-27 09:12:59 +1100

    Second Commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()  {
     float y;
     printf("Enter a number: ");
     scanf("%f", &x); //Note the change of %d to %f
-    y = (x+2)/(x-1);
+    y = x*2 + 5 + 6 + 2 + 1;
     printf("y: %f\n", y);
     return 0;
 }
